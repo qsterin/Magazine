f3a2b99011e7a738aa3e7280a3c7a17035898dac
 Eвгений Костерин <qsterin@gmail.com>
2022-08-29 17:57:24 +0300

    initial

---
+++ src/main/java/Repository.java
@@ -0,0 +1,47 @@
+public class Repository {
+    protected Product[] products = new Product[0];
+
+    public void add(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+    public Product findById(int id) {
+        for (Product product: products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void removeByID(int id) throws NotFoundException {
+        if (findById(id) == null) {
+           throw new NotFoundException(
+                    "ID " + id + "не найден");
+           // System.out.println( "ID " + id + " не найден");
+        }
+
+
+
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product: products) {
+            if (product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+                products = tmp;
+            }
+        }
+    }
+
+    public Product[] findAll() {
+        return products;
+    }
+
+}
+
